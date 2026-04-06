f1ae8b3bfe5f8027bcd9af86d29a82879c34e8ee
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2026-04-06 20:47:37 -0300

    Update app.routes.ts

--- portal/src/app/app.routes.ts
+++ portal/src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { Internacional } from './internacional/internacional';
 export const routes: Routes = [
     {path:'local', component:Local},
     {path:'regional', component:Regional},
-    {path:'internacional', component:Internacional},
+    {path:'internacional', component:Internacional}
 ];
