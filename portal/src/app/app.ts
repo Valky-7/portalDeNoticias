dba2aa76d3d0abb186ffb7345a097e8b1f6b78d0
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2026-04-06 20:43:22 -0300

    Removido componentes local/regional/internacional
    Remove unused regional, local and internacional components from the root app. Deleted their tags from portal/src/app/app.html and removed the corresponding imports and entries from the @Component imports array in portal/src/app/app.ts, leaving RouterOutlet, Header, Footer and Home only.

--- portal/src/app/app.ts
+++ portal/src/app/app.ts
@@ -3,13 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { Home } from './home/home';
-import { Regional } from './regional/regional';
-import { Internacional } from './internacional/internacional';
-import { Local } from './local/local';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Home, Regional, Internacional, Local],
+  imports: [RouterOutlet, Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
