89f2abebb5a30b40d8118c3a22c679484ddc3c5d
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2026-04-06 20:21:56 -0300

    Update app.ts

--- portal/src/app/app.ts
+++ portal/src/app/app.ts
@@ -2,14 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
-import { Local } from './local/local';
+import { Home } from './home/home';
 import { Regional } from './regional/regional';
 import { Internacional } from './internacional/internacional';
-import { Home } from './home/home';
+import { Local } from './local/local';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Local, Regional, Internacional, Home],
+  imports: [RouterOutlet, Header, Footer, Home, Regional, Internacional, Local],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
